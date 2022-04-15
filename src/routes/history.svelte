b342a18b51b7d3cc0b6f70f36766757bd2754478
L1ghtee <nikorak20@gmail.com>
2022-04-15 17:29:49 +0300

    add json with data

--- src/routes/history.svelte
+++ src/routes/history.svelte
@@ -10,14 +10,18 @@
 <section class="text-gray-600 body-font">
     <div class="container px-5 py-24 mx-auto flex flex-col">
       <div class="lg:w-5/6 mx-auto">
-        <div class="rounded-lg h-64 overflow-hidden">
-          <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1200x500">
+        <div class="rounded-lg h-100 overflow-hidden">
+          <img alt="content" class="object-cover object-center h-full w-full" src="static\gallary\38071411_268663153864021_3592749785305579520_n.jpg">
         </div>
         <div class="flex flex-col sm:flex-row mt-10">
          
           <div class="sm:w-3/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-            <p class="leading-relaxed text-lg mb-4">Meggings portland fingerstache lyft, post-ironic fixie man bun banh mi umami everyday carry hexagon locavore direct trade art party. Locavore small batch listicle gastropub farm-to-table lumbersexual salvia messenger bag. Coloring book flannel truffaut craft beer drinking vinegar sartorial, disrupt fashion axe normcore meh butcher. Portland 90's scenester vexillologist forage post-ironic asymmetrical, chartreuse disrupt butcher paleo intelligentsia pabst before they sold out four loko. 3 wolf moon brooklyn.</p>
-
+            <p class="leading-relaxed text-lg mb-4">Вогонь знищив її у 2003-му... Сумна сторінка в історії церкви... </p>
+            <img src="https://scontent.flwo6-1.fna.fbcdn.net/v/t1.6435-9/37992736_268660453864291_6556373615791570944_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=b9115d&_nc_ohc=IYBBen4iUGgAX-pL_N_&_nc_oc=AQn1NUq7gVZoQFiGMQfHChMAuNQe_Oc-9LZ1q-z_0Jui_jOw3WKo1lPfw_tYwvy9P90&tn=Fka4YhGJJ6rkytSJ&_nc_ht=scontent.flwo6-1.fna&oh=00_AT9mbMHtVsWaVG-4I9d44wJVJA-5C9MqzNydJm7PNXZJFg&oe=6280DC61" alt=""style:width=50%>
+            <p class="leading-relaxed text-lg mb-4">Заболотівський декан о.Василь Гоян, у співслужінні духовного пастиря церкви Святого Пророка Іллі Мирослава Сметанюка та запрошених отців, благословив святе місце під нову церкву. Благовіщення, 2011 р. Б. </p> 
+            <img src="https://scontent.flwo6-1.fna.fbcdn.net/v/t1.6435-9/38120576_268660813864255_7575970161480433664_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=b9115d&_nc_ohc=FdNPTubu-50AX_zIB4m&_nc_ht=scontent.flwo6-1.fna&oh=00_AT_PlWWSE8K0cAUDEhFfXWcELdKCoAdAxQsYAfulgZbtGA&oe=627F2196" alt=""style:width=50% >
+            <p class="leading-relaxed text-lg mb-4">Новозбудована святиня - ще пахне живицею й зігріта теплом людських рук. 2013 р. Б. </p>
+            <img src="https://scontent.flwo6-1.fna.fbcdn.net/v/t1.6435-9/37999925_268663857197284_7540521846471393280_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=b9115d&_nc_ohc=YfLIbLCSb6AAX-DBznz&_nc_ht=scontent.flwo6-1.fna&oh=00_AT-o1IF0mH_0cj0H0MFapdGn5kgepoBCkoC_n2JVqZIJ8Q&oe=6280B761" alt=""style:width=50%>
           </div>
         </div>
       </div>
